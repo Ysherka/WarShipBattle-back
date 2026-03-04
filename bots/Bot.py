f32774606204a6afd0ee7d30ea28d8fc636b1d41
ITsviliy <ivantsviliy@gmail.com>
2026-03-04 20:55:41 +0400

    зачатки ботиков

---
+++ bots/Bot.py
@@ -0,0 +1,33 @@
+import random
+from typing import List, Tuple, Optional, Set
+from models.Field import OwnField
+from models.Field import EnemyField
+from models.Ship import Ship
+from models.ShipOrientation import ShipOrientation
+from models.FieldState import FieldState
+from placements.RandomShipPlacer import RandomShipPlacer
+
+
+class Bot:
+    
+    def __init__(self, name: str = "Bot", field_size: int = 10):
+        self.name = name
+        self.field_size = field_size
+        self.my_field = OwnField(field_size)
+        self.enemy_view = OwnField(field_size) 
+        self.ships_placed = False
+        
+    def place_ships(self) -> None:
+        placer = RandomShipPlacer(self.field_size)
+        ships = placer.place_ships_randomly()
+        
+        for ship in ships:
+            self.my_field.add_ship(ship)
+        
+        self.ships_placed = True
+        print(f"🤖 {self.name} расставил корабли")
+    
+    def make_move(self) -> Tuple[int, int]:
+
+        raise NotImplementedError
+    
---
+++ bots/RandomBot.py
@@ -0,0 +1,13 @@
+from Bot import *
+import random
+class RandomBot(Bot):
+    
+    def __init__(self, name: str = "RandomBot", field_size: int = 10):
+        super().__init__(name, field_size)
+        
+    
+    def make_move(self) -> Tuple[int, int]:
+        
+    
+        
+        return x, y
